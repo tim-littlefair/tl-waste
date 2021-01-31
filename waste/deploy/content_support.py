4fd0a5043f5fa6e4c08917600bc1de0dd58324c3
Tim Littlefair <5130151+tim-littlefair@users.noreply.github.com>
2021-01-31 13:27:43 +0800

    Changes required to get all tests passing on Windows 10.

--- waste/deploy/content_support.py
+++ waste/deploy/content_support.py
@@ -11,11 +11,12 @@ def content_dir_to_in_memory_zip_stream(content_dir):
     in_memory_zip_stream = io.BytesIO()
     with zipfile.ZipFile(in_memory_zip_stream,"w") as in_memory_zip:
         for walk_path, subdir_names, file_basenames in os.walk(content_dir):
+            walk_path = walk_path.replace("\\","/")
             for fbn in file_basenames:
-                file_relpath = os.path.join(walk_path,fbn).replace(content_dir+"/","")
+                file_relpath = "/".join([walk_path,fbn]).replace(content_dir+"/","")
                 logging.info("Adding %s to zip",file_relpath)
                 in_memory_zip.write(
-                    os.path.join(content_dir, file_relpath),
+                    "/".join([content_dir, file_relpath]),
                     arcname = "/" + file_relpath
             )
         namelist = in_memory_zip.namelist()
